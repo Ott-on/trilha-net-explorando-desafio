424d1bcd2a39b432a00a7dc95d0a553ef77f82fd
Ott-on <ottonvinicius1004@gmail.com>
2024-05-12 21:52:35 -0300

    feat: Enum TiposDeSuite

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,17 +1,25 @@
 namespace DesafioProjetoHospedagem.Models
-{
+{   
+    public enum TiposDeSuite
+    {
+        Standard,
+        Econômica,
+        Familiar,
+        Duplex,
+        Compacta
+    }
     public class Suite
     {
         public Suite() { }
 
-        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        public Suite(TiposDeSuite tipoSuite, int capacidade, decimal valorDiaria)
         {
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
 
-        public string TipoSuite { get; set; }
+        public TiposDeSuite TipoSuite { get; set; }
         public int Capacidade { get; set; }
         public decimal ValorDiaria { get; set; }
     }
