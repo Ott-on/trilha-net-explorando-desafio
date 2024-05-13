6c8fe9871f8ec74e26731dfd5ed83cbc8116d057
Ott-on <ottonvinicius1004@gmail.com>
2024-05-12 23:14:34 -0300

    fix: Remove Enum Suite

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,25 +1,18 @@
 namespace DesafioProjetoHospedagem.Models
 {   
-    public enum TiposDeSuite
-    {
-        Standard,
-        Econômica,
-        Familiar,
-        Duplex,
-        Compacta
-    }
+
     public class Suite
     {
         public Suite() { }
 
-        public Suite(TiposDeSuite tipoSuite, int capacidade, decimal valorDiaria)
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
         {
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
 
-        public TiposDeSuite TipoSuite { get; set; }
+        public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
         public decimal ValorDiaria { get; set; }
     }
